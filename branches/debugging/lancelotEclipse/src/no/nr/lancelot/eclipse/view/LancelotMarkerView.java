3c806573d0e61931971a6d5c12126f831f2be577
edvard.kristoffer.karlsen@gmail.com <edvard.kristoffer.karlsen@gmail.com@85a358d8-25f7-b76e-27b6-fbb467d5d4d4>
2011-08-28 22:05:24 +0000

    Removed Debugging branch

--- branches/debugging/lancelotEclipse/src/no/nr/lancelot/eclipse/view/LancelotMarkerView.java
+++
@@ -1,9 +0,0 @@
-package no.nr.lancelot.eclipse.view;
-
-import org.eclipse.ui.views.markers.MarkerSupportView;
-
-public class LancelotMarkerView extends MarkerSupportView {
-    public LancelotMarkerView() {
-        super("no.nr.lancelot.eclipse.view.markerViewContentGenerator");
-    }
-}
