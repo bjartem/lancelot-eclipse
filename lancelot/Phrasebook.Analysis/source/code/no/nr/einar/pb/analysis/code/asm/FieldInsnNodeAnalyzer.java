e8102308a3d359c2df0d006da52175a10a0632b6
edvard.kristoffer.karlsen <edvard.kristoffer.karlsen@gmail.com>
2011-09-01 11:31:49 +0000

    United source folders. Moved test to separate project.

--- lancelot/Phrasebook.Analysis/source/code/no/nr/einar/pb/analysis/code/asm/FieldInsnNodeAnalyzer.java
+++
@@ -1,9 +0,0 @@
-package no.nr.einar.pb.analysis.code.asm;
-
-import no.nr.einar.pb.model.Attribute;
-
-public interface FieldInsnNodeAnalyzer extends InstructionNodeAnalyzer {
-
-	Attribute getAttribute();
-	
-}
--- lancelot/Phrasebook.Analysis/source/code/no/nr/einar/pb/analysis/code/descriptor/ITypeParser.java
+++
@@ -1,9 +0,0 @@
-package no.nr.einar.pb.analysis.code.descriptor;
-
-public interface ITypeParser {
-
-	boolean parse(final int firstCharIndex, final char[] chars);
-	String getTypeName();
-	int getIndexOfLastCharRead();
-
-}
