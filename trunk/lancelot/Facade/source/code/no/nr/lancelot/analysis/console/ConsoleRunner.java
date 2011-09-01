a5dde093c8da2a9ee74eed985baddd918cfa9de0
edvard.kristoffer.karlsen@gmail.com <edvard.kristoffer.karlsen@gmail.com@85a358d8-25f7-b76e-27b6-fbb467d5d4d4>
2011-09-01 11:31:49 +0000

    United source folders. Moved test to separate project.

--- trunk/lancelot/Facade/source/code/no/nr/lancelot/analysis/console/ConsoleRunner.java
+++
@@ -1,6 +0,0 @@
-package no.nr.lancelot.analysis.console;
-
-public class ConsoleRunner {
-	public static void main(String[] args) {
-	}
-}
--- trunk/lancelot/Rulebook/source/code/no/nr/einar/naming/rulebook/IRulebook.java
+++
@@ -1,7 +0,0 @@
-package no.nr.einar.naming.rulebook;
-
-import java.util.Set;
-
-public interface IRulebook {
-	Set<Rule> findViolations(final MethodIdea methodIdea);
-}
