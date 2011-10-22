22a0ead4f10193a1fb5ef974338896ae351e7851
edvard.kristoffer.karlsen@gmail.com <edvard.kristoffer.karlsen@gmail.com@85a358d8-25f7-b76e-27b6-fbb467d5d4d4>
2011-10-22 12:27:22 +0000

    I have to do a re-import.

--- trunk/sandbox/galahad/run.sh
+++
@@ -1,4 +0,0 @@
-#JAVA_OPTS=-Xprof 
-CLASSPATH=$CLASSPATH:./lib/javaparser-1.0.8.jar:../../lancelot/bin:../../lancelot/lib/annotations.jar:../../lancelot/lib/asm-3.0.jar:../../lancelot/lib/asm-analysis-3.0.jar:../../lancelot/lib/asm-tree-3.0.jar:../../lancelot/lib/edu.mit.jwi_2.1.4_jdk.jar:../../lancelot/lib/jsr305.jar:../../lancelot/lib/xom-1.1.jar javac galahadjava/Tracker.java
-CLASSPATH=$CLASSPATH:./lib/javaparser-1.0.8.jar:../../lancelot/bin:../../lancelot/lib/annotations.jar:../../lancelot/lib/asm-3.0.jar:../../lancelot/lib/asm-analysis-3.0.jar:../../lancelot/lib/asm-tree-3.0.jar:../../lancelot/lib/edu.mit.jwi_2.1.4_jdk.jar:../../lancelot/lib/jsr305.jar:../../lancelot/lib/xom-1.1.jar time jython main.py
-
